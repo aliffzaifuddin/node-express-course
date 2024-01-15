9934628c3f2c0c0087325f171fa0aa19b3441996
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-01-15 14:59:54 +0000

    add error handler and customer error message

---
+++ 03-task-manager/README.md
@@ -0,0 +1,7 @@
+# In this project, we learn that
+
+- to setup and connect to cloud database
+- to perform CRUD operations (create, read, update, delete) on our data
+- this is only back end project, all front end already had been prepared
+
+All the files and starter codes are owned by John Smilga and FreeCodeCamp and edited by me for the learning of Nodejs, Express and this project
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,65 +1,46 @@
 const Task = require('../models/Task')
+const asyncWapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
-const getAllTasks = async (req,res) => {
-  try {
-    const tasks = await Task.find({})
-    res.status(200).json({ tasks })
-  } catch (error) {
-    res.status(500).json({ msg: error})
-  }
-}
+const getAllTasks = asyncWapper(async (req,res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
-const createTask = async (req,res) => {
-  try {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-    
-  } catch (error) {
-    res.status(500).json({ msg: error})
-  }
-}
+const createTask = asyncWapper(async (req,res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({task})
+})
 
-const getTask = async (req,res) => {
-  try {
-    const {id: taskID} = req.params
-    const task = await Task.findOne({ _id:taskID})
-    if(!task) {
-      return res.status(404).json({ msg:`No task with id : ${taskID}`})
-    }
-    res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error})
+const getTask = asyncWapper(async (req,res,next) => {
+  const {id: taskID} = req.params
+  const task = await Task.findOne({ _id:taskID})
+  if(!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-}
+  res.status(200).json({ task })
+})
 
-const deleteTask = async (req,res) => {
-  try {
-    const {id: taskID} = req.params
-    const task = await Task.findOneAndDelete({ _id:taskID })
-    if (!task) {
-      return res.status(404).json({ msg:`No task with id : ${taskID}`})
-    }
-    res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error})
+const deleteTask = asyncWapper(async (req,res,next) => {
+  const {id: taskID} = req.params
+  const task = await Task.findOneAndDelete({ _id:taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-}
+  res.status(200).json({ task })
+})
 
-const updateTask = async (req,res) => {
-  try {
-    const {id:taskID} = req.params
-    const task = await Task.findOneAndUpdate({ _id: taskID} , req.body, {
-      new: true, 
-      runValidators: true,
-    })
-    if (!task) {
-      return res.status(404).json({ msg:`No task with id : ${taskID}`})
-    }
-    res.status(200).json({task})
-  } catch (error) {
-    res.status(500).json({ msg: error})
+const updateTask = asyncWapper(async (req,res,next) => {
+  const {id:taskID} = req.params
+  const task = await Task.findOneAndUpdate({ _id: taskID} , req.body, {
+    new: true, 
+    runValidators: true,
+  })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-}
+  res.status(200).json({task})
+})
 
 module.exports = {
 	getAllTasks,
