697748b1a6d9597e7901077f093c80ded6730a25
Aliff Zaifuddin <aliffzaifuddin97@gmail.com>
2024-01-16 14:06:38 +0000

    add numericFilter method to filter price and rating

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,77 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req,res) => {
-  throw new Error('testing async errors')
-  res.status(200).json({ msg:'products testing route' })
+  const products = await Product.find({ price : {$gt:30} })
+    .sort('price')
+    .select('name price')
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req,res) => {
-  res.status(200).json({ msg:'products route' })
+  const {featured , company, name, sort, fields, numericFilters } = req.query
+  const queryObject = {}
+  // to filter r=products by featured
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  // to filter products by company name
+  if (company) {
+    queryObject.company = company
+  }
+  // to filter products by name 
+  if (name) {
+    queryObject.name = { $regex : name, $options: 'i' }
+  }
+
+  if(numericFilters) {
+    const operatorMap = {
+      '>':'$gt',
+      '>=':'$gte',
+      '=':'$eq',
+      '<':'$lt',
+      '<=':'$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx, 
+      (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price','rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]:Number(value) }
+      }
+    })
+  }
+
+  console.log(queryObject)
+
+  let result = Product.find(queryObject)
+  // to sort products
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  // select certain fields on products
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  // setup page, limit and skip to create pagination 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1 ) * limit
+
+  result = result.skip(skip).limit(limit)
+
+
+  const products = await result
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 
